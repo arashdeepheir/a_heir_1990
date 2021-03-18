f2a398283adc9fc4474b1c477ccfc5d7437e3b3e
arashdeepheir <65856287+arashdeepheir@users.noreply.github.com>
2021-03-18 09:09:40 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int sum,num1,num2;
+	
+	cout<<"Enter the value of first number:";
+	cin>>num1;
+	cout<<"\n";
+	
+	cout<<"Enter the value of second number:";
+	cin>>num2;
+	cout<<"\n";
+	
+	sum=num1+num2;
+	
+	cout<<"Sum of two numbers :";
+	cout<<sum;
+	
+	return 0;
+	
+}
