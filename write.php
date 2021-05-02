2cdd84e9edefdd0cef1354739c19771a35d84efe
arashdeepheir <65856287+arashdeepheir@users.noreply.github.com>
2021-05-02 08:55:13 +0530

    Add files via upload

---
+++ write.php
@@ -0,0 +1,13 @@
+<?php
+
+$myfile = fopen("page1.html", "w") or die("Unable to open file!");
+$txt1 = sscanf($str,"mobileno:%d",$mobileno);
+fwrite($myfile, $txt1);
+
+$form=array("mobileno");
+echo count($form);
+echo fread($myfile);
+echo fwrite($myfile);
+//fclose($myfile);
+
+?>
