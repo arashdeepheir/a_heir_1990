017c3d25e7d17f17a09d976ccf66b158c313605f
ArashdeepHeir <65856287+arashdeepheir@users.noreply.github.com>
2021-08-07 10:20:05 +0530

    Add files via upload

---
+++ Quad_equation.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author arash
+ */
+import java.util.Scanner;
+import java.lang.Math;
+public class Quad_equation {
+    
+	 
+    public static void main(String[] args)
+    {
+    	
+    	double num1,num2,num3,alpha,beta,a,b,c;
+        double d,det1,det2,det3;
+        int ch;
+        
+        do    
+        {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\nDo you want to run the program?(press 1 to enter/press 2 to exit)");
+                  
+ 
+            ch = scan.nextInt();            
+            switch (ch)
+            {
+		
+            case 1 : 
+            {
+                System.out.println("Enter variables of Quadratic equation ax^2+bx+c=0 \n");
+            	
+            	a = scan.nextDouble();
+                b = scan.nextDouble();
+                c = scan.nextDouble();
+
+            	System.out.println("\n:: Quadratic Equation is " + a +"x^2+"+ b +"x+" + c +"=0" );
+                
+                det1=(b*b);
+		det2=(4*a*c);
+		det3=det1-det2;
+                
+            	System.out.println("\n Determinent of the Quadratic Equation is "+ det3);
+                
+            	d=Math.sqrt(det3);
+                
+                num1=-b+d;
+                num2=2*a;
+                num3=-b-d;
+                alpha=num1/num2;
+                beta=num3/num2;
+                System.out.println("\n Roots of the Quadratic Equation are " + alpha + "\t"+ beta);
+               
+            }   
+                                      
+            case 2 :
+            	{
+            	System.exit(0);  
+            	break;                        
+            
+            	}
+            	}
+ 
+            	} while(true);  
+    }
+}
